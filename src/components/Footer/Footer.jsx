9a71594afc857682fabbff3bb0a4d1d162278478
Oleksandr Veryk <veryk.av@gmail.com>
2025-07-27 23:41:05 +0300

    Added tyling for active NavLinks from Tailwind

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Container } from "../Container/Container";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white mt-8">
+    <footer className="bg-gray-800 text-white">
       <Container>
         <p className="p-4">&copy; 2025 Filmial. All rights reserved.</p>
       </Container>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Container } from "../Container/Container";
-import { Link, Links } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
+  const isActiveLink = ({ isActive }) =>
+    isActive
+      ? "p-2 text-blue-300 font-semibold underline underline-offset-8 decoration-blue-300"
+      : "p-2 text-white font-semibold  hover:text-blue-300 hover:underline hover:underline-offset-8 hover:decoration-blue-300  transition";
+
   return (
-    <header className="bg-blue-600 text-white ">
+    <header className="bg-gray-800 text-white ">
       <Container>
         <div className="">
           <h1 className="text-xl font-semibold p-4">Filmial</h1>
-          <nav>
-            <Link to="/">Головна</Link>
-            <Link to="/movies">Фільми</Link>
-            <Link to="/serials">Серіали</Link>
+          <nav className="flex gap-[12px] text-base">
+            <NavLink className={isActiveLink} to="/">
+              Головна
+            </NavLink>
+            <NavLink className={isActiveLink} to="/movies">
+              Фільми
+            </NavLink>
+            <NavLink className={isActiveLink} to="/serials">
+              Серіали
+            </NavLink>
           </nav>
         </div>
       </Container>
