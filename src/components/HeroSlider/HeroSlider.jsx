8055143b6f5c9f614aa17d357107b3f0b3a1d5d3
Oleksandr Veryk <veryk.av@gmail.com>
2025-09-07 23:50:34 +0300

    update slider

--- src/components/HeroSlider/HeroSlider.jsx
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -10,19 +10,29 @@ export const HeroSlider = ({ popularMovies }) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
   };
   return (
-    <Slider {...settings} className="mb-12">
-      {popularMovies.map((popularMovie) => (
-        <div key={popularMovie.id}>
-          <img
-            className="rounded-xl"
-            src={`https://image.tmdb.org/t/p/original/${popularMovie.backdrop_path}`}
-            alt={popularMovie.title}
-          />
-          <h3 className="font-mono text-2xl p-2">{popularMovie.title}</h3>
-        </div>
-      ))}
-    </Slider>
+    <div className=" w-full">
+      <Slider {...settings} className="w-full mb-12 ">
+        {popularMovies.map((popularMovie) => (
+          <div key={popularMovie.id} className="w-full relative">
+            {console.log(popularMovie)}
+            <img
+              className="w-full h-[450px] object-cover rounded-xl"
+              src={`https://image.tmdb.org/t/p/original/${popularMovie.backdrop_path}`}
+              alt={popularMovie.title}
+            />
+            <h3
+              className="font-mono text-2xl p-2 absolute bottom-0 left-0 bg-amber-50/40  w-full rounded-xl
+            ">
+              {popularMovie.title}
+            </h3>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
